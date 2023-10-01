08e4698a7c285e9321259908183c6ff77aae2ac3
Sabina Sadykova <sabik8711@gmail.com>
2023-10-01 21:45:07 +0300

    Наука о данных

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,19 @@
+a=int( 78 )
+b=float( 9.8)
+c=bool( True)
+id(a)
 
+a=input("Введите число:")
+if a.isdigit():
+    seconds=int(a)
+    minutes=seconds/60
+    hours=minutes/60
+    print (seconds)
+    print(minutes)
+    print(hours)
+
+
+a=input("Введите число от 1 до 9: ")
+if a.isdigit() and 0<int(a)<10:
+    b=int(a)+int(a+a)+int(a+a+a)
+    print(b)
